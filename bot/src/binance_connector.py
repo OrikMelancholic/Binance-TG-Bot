3a42cd9f9f9e95f6595b77517990def1a6d816ec
Skudarilia <skudarilya@gmail.com>
2021-12-21 09:45:28 +0300

    Добавил функцию вывода текущей цены и построение графика

--- bot/src/binance_connector.py
+++ bot/src/binance_connector.py
@@ -2,6 +2,7 @@ from config import config as conf
 from binance import Client
 from datetime import datetime
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 class BinanceConnector:
@@ -69,3 +70,19 @@ class BinanceConnector:
         df['median'] = (df['High'] + df['Low']) / 2
 
         return df
+
+    def get_price_index(self, pair):
+        # Parameters:	symbol (str) – name of the symbol pair
+        # symbol='BTCUSDT'
+        return self.client.get_margin_price_index(symbol=pair)
+
+    def plot(self, time, data):
+        # Parameters:
+        #   time (list of timestamps) - X axis
+        #   data (list of values) - Y data
+        plt.subplot(1, 1, 1)
+        plt.plot(time, data)
+        plt.gcf().autofmt_xdate()
+        plt.savefig('../figures/test_fig.png')
+        plt.close()
+
