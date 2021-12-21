9b88f9057985f689d9900689f1dc10aeae872c2f
Skudarilia <skudarilya@gmail.com>
2021-12-21 13:35:27 +0300

    Добавил логику кнопки назад

--- bot/src/bot.py
+++ bot/src/bot.py
@@ -1,11 +1,10 @@
 import telebot
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from bot.src.plot_params import PlotParams
-from bot.config import config as configs
-from bot.config import markets as markets_list
-
-from bot.src.binance_connector import BinanceConnector
+from config import markets as markets_list
+from src.PlotParams import PlotParams
+from config import config as configs
+from src.binance_connector import BinanceConnector
 from datetime import datetime, timedelta
 
 plot_parameters = PlotParams()
@@ -28,22 +27,23 @@ def start(message):
     menu.add(InlineKeyboardButton(text="Избранное", callback_data="fvrt"))
 
     bot_message = bot.send_message(chat_id=message.chat.id,
-                                   text="Добро пожаловать.\nЭтот бот поможет в работе с Binance!",
-                                   reply_markup=menu)
+                     text="Добро пожаловать.\nЭтот бот поможет в работе с Binance!",
+                     reply_markup=menu)
     last_bot_message_id = bot_message.message_id
 
 
 def main_menu():
     global chat_id
     global last_bot_message_id
+    plot_parameters.current_stage = -1
 
     menu = InlineKeyboardMarkup()
     menu.add(InlineKeyboardButton(text="Рынок", callback_data="mrkt"))
     menu.add(InlineKeyboardButton(text="Избранное", callback_data="fvrt"))
 
     bot_message = bot.send_message(chat_id=chat_id,
-                                   text="Добро пожаловать.\nЭтот бот поможет в работе с Binance!",
-                                   reply_markup=menu)
+                     text="Добро пожаловать.\nЭтот бот поможет в работе с Binance!",
+                     reply_markup=menu)
     last_bot_message_id = bot_message.message_id
 
 
@@ -57,29 +57,215 @@ def market_selection(query):
         bot.delete_message(chat_id=chat_id, message_id=last_bot_message_id)
         show_markets_list()
     elif "mrkt_1" in query.data:
-        market = query.data.replace('mrkt_1_', '')
-        print(f"market => {market}")
-        bot.edit_message_text(chat_id=chat_id, message_id=last_bot_message_id, text=f"Выбранный рынок: {market}")
-        show_currencies_list()
+        if query.data == "mrkt_1_back":
+            bot.delete_message(chat_id=chat_id, message_id=last_bot_message_id)
+            main_menu()
+        else:
+            market = query.data.replace('mrkt_1_', '')
+            print(f"market => {market}")
+            bot.edit_message_text(chat_id=chat_id, message_id=last_bot_message_id, text=f"Выбранный рынок: {market}")
+            plot_parameters.market = market
+            show_currencies_list()
     elif "mrkt_2" in query.data:
-        currency = query.data.replace('mrkt_2_', '')
-        print(f"currency => {currency}")
-        bot.edit_message_text(chat_id=chat_id, message_id=last_bot_message_id, text=f"Выбранная валюта: {currency}")
-        plot_parameters.currency = currency
-        show_intervals_list()
+        if query.data == "mrkt_2_back":
+            bot.delete_message(chat_id=chat_id, message_id=last_bot_message_id)
+            show_markets_list()
+        else:
+            currency = query.data.replace('mrkt_2_', '')
+            print(f"currency => {currency}")
+            bot.edit_message_text(chat_id=chat_id, message_id=last_bot_message_id, text=f"Выбранная валюта: {currency}")
+            plot_parameters.currency = currency
+            show_intervals_list()
     elif "mrkt_3" in query.data:
-        days_count = int(query.data.replace('mrkt_3_', ''))
-        print(f"days_count => {days_count}")
-        bot.edit_message_text(chat_id=chat_id, message_id=last_bot_message_id,
-                              text=f"Интервал для графика: {days_count} дней")
-        plot_parameters.date_interval = days_count
-        show_candles_list()
+        if query.data == "mrkt_3_back":
+            bot.delete_message(chat_id=chat_id, message_id=last_bot_message_id)
+            show_currencies_list()
+        else:
+            days_count = int(query.data.replace('mrkt_3_', ''))
+            print(f"days_count => {days_count}")
+            bot.edit_message_text(chat_id=chat_id, message_id=last_bot_message_id, text=f"Интервал для графика: {days_count} дней")
+            plot_parameters.date_interval = days_count
+            show_candles_list()
     elif "mrkt_4" in query.data:
-        candle_size = query.data.replace('mrkt_4_', '')
-        print(f"candle_size => {candle_size}")
-        bot.edit_message_text(chat_id=chat_id, message_id=last_bot_message_id, text=f"Ширина свечи: {candle_size}")
-        plot_parameters.candle_size = candle_size
-        show_results()
+        if query.data == "mrkt_4_back":
+            bot.delete_message(chat_id=chat_id, message_id=last_bot_message_id)
+            show_intervals_list()
+        else:
+            candle_size = query.data.replace('mrkt_4_', '')
+            print(f"candle_size => {candle_size}")
+            bot.edit_message_text(chat_id=chat_id, message_id=last_bot_message_id, text=f"Ширина свечи: {candle_size}")
+            plot_parameters.candle_size = candle_size
+
+            show_results()
+    elif "mrkt_reset" in query.data:
+        plot_parameters.reset()
+        main_menu()
+
+
+def show_markets_list():
+    global last_bot_message_id
+    plot_parameters.current_stage = 1
+    message = "Выберите интересующий Вас рынок:"
+    markets = ["BNB", "BTC", "ETH", "AUD", "RUB"]
+    market_buttons = []
+
+    for market_name in markets:
+        market_buttons.append([InlineKeyboardButton(market_name, callback_data=f"mrkt_1_{market_name}")])
+    market_buttons.append([InlineKeyboardButton("Назад", callback_data=f"mrkt_1_back")])
+
+    bot_message = bot.send_message(chat_id=chat_id,
+                                   text=message,
+                                   reply_markup=InlineKeyboardMarkup(market_buttons))
+    last_bot_message_id = bot_message.message_id
+
+
+def show_intervals_list():
+    global last_bot_message_id
+    plot_parameters.current_stage = 3
+    message = "Выберите интервал для графика:"
+
+    interval_buttons = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "День", callback_data="mrkt_3_01"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "3 дня", callback_data="mrkt_3_03"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "Неделя", callback_data="mrkt_3_07"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "Месяц", callback_data="mrkt_3_30"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "Три месяца", callback_data="mrkt_3_90"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "Назад", callback_data="mrkt_3_back"
+                )
+            ]
+        ]
+    )
+
+    bot_message = bot.send_message(chat_id=chat_id,
+                                   text=message,
+                                   reply_markup=interval_buttons)
+    last_bot_message_id = bot_message.message_id
+
+
+def show_currencies_list():
+    global last_bot_message_id
+    plot_parameters.current_stage = 2
+    message = "Выберите валюту либо введите её код.\nПрим.: BTC"
+    selected_market_code = plot_parameters.market
+    if selected_market_code == "BNB":
+        markets = ["ADA", "CREAM", "FARM", "CAKE", "SAND"]
+    elif selected_market_code == "BTC":
+        markets = ["DUSK", "DASH", "ATM", "1INCH", "POLY"]
+    elif selected_market_code == "ETH":
+        markets = ["BNB", "ETC", "HOT", "MFT", "NANO"]
+    elif selected_market_code == "AUD":
+        markets = ["BNB", "BTC", "ETH", "XRP", "LUNA"]
+    elif selected_market_code == "RUB":
+        markets = ["BNB", "BTC", "DOGE", "ETH", "USDT"]
+    currency_buttons = []
+
+    for currency_name in markets:
+        currency_buttons.append([InlineKeyboardButton(currency_name, callback_data=f"mrkt_2_{currency_name}")])
+    currency_buttons.append([InlineKeyboardButton("Назад", callback_data=f"mrkt_2_back")])
+
+    bot_message = bot.send_message(chat_id=chat_id,
+                                   text=message,
+                                   reply_markup=InlineKeyboardMarkup(currency_buttons))
+    last_bot_message_id = bot_message.message_id
+
+
+def show_candles_list():
+    global last_bot_message_id
+    plot_parameters.current_stage = 4
+    message = "Выберите ширину свечи для графика:"
+
+    candle_buttons_std = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "15 Минут", callback_data="mrkt_4_15m"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "1 Час", callback_data="mrkt_4_1h"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "4 Часа", callback_data="mrkt_4_4h"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "12 Часов", callback_data="mrkt_4_12h"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "1 День", callback_data="mrkt_4_1d"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "Назад", callback_data="mrkt_4_back"
+                )
+            ]
+        ]
+    )
+
+    candle_buttons_sml = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "15 Минут", callback_data="mrkt_4_15m"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "1 Час", callback_data="mrkt_4_1h"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "4 Часа", callback_data="mrkt_4_4h"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "Назад", callback_data="mrkt_4_back"
+                )
+            ]
+        ]
+    )
+
+    if plot_parameters.date_interval == 1:
+        buttons = candle_buttons_sml
+    else:
+        buttons = candle_buttons_std
+
+    bot_message = bot.send_message(chat_id=chat_id,
+                                   text=message,
+                                   reply_markup=buttons)
+    last_bot_message_id = bot_message.message_id
 
 
 def show_results():
@@ -131,154 +317,6 @@ def show_results():
                                  caption=f"График изменения цены по следующим параметрам:\n"
                                          f"{plot_parameters.currency}/{plot_parameters.market}"
                                  )
-    last_bot_message_id = bot_message.message_id
-
-
-def show_markets_list():
-    global last_bot_message_id
-    message = "Выберите интересующий Вас рынок:"
-    markets = ["BNB", "BTC", "ETH", "AUD", "RUB"]
-    market_buttons = []
-
-    for market_name in markets:
-        market_buttons.append([InlineKeyboardButton(market_name, callback_data=f"mrkt_1_{market_name}")])
-
-    bot_message = bot.send_message(chat_id=chat_id,
-                                   text=message,
-                                   reply_markup=InlineKeyboardMarkup(market_buttons))
-    last_bot_message_id = bot_message.message_id
-
-
-def show_currencies_list():
-    global last_bot_message_id
-    plot_parameters.current_stage = 2
-    message = "Выберите валюту либо введите её код.\nПрим.: BTC"
-    selected_market_code = plot_parameters.market
-    if selected_market_code == "BNB":
-        markets = ["ADA", "CREAM", "FARM", "CAKE", "SAND"]
-    elif selected_market_code == "BTC":
-        markets = ["DUSK", "DASH", "ATM", "1INCH", "POLY"]
-    elif selected_market_code == "ETH":
-        markets = ["BNB", "ETC", "HOT", "MFT", "NANO"]
-    elif selected_market_code == "AUD":
-        markets = ["BNB", "BTC", "ETH", "XRP", "LUNA"]
-    elif selected_market_code == "RUB":
-        markets = ["BNB", "BTC", "DOGE", "ETH", "USDT"]
-    currency_buttons = []
-
-    for currency_name in markets:
-        currency_buttons.append([InlineKeyboardButton(currency_name, callback_data=f"mrkt_2_{currency_name}")])
-
-    bot_message = bot.send_message(chat_id=chat_id,
-                                   text=message,
-                                   reply_markup=InlineKeyboardMarkup(currency_buttons))
-    last_bot_message_id = bot_message.message_id
-
-
-def show_intervals_list():
-    global last_bot_message_id
-    plot_parameters.current_stage = 3
-    message = "Выберите интервал для графика:"
-
-    interval_buttons = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "День", callback_data="mrkt_3_01"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "3 дня", callback_data="mrkt_3_03"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "Неделя", callback_data="mrkt_3_07"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "Месяц", callback_data="mrkt_3_30"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "Три месяца", callback_data="mrkt_3_90"
-                )
-            ]
-        ]
-    )
-
-    bot_message = bot.send_message(chat_id=chat_id,
-                                   text=message,
-                                   reply_markup=interval_buttons)
-    last_bot_message_id = bot_message.message_id
-
-
-def show_candles_list():
-    global last_bot_message_id
-    plot_parameters.current_stage = 4
-    message = "Выберите ширину свечи для графика:"
-
-    candle_buttons_std = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "15 Минут", callback_data="mrkt_4_15m"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "1 Час", callback_data="mrkt_4_1h"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "4 Часа", callback_data="mrkt_4_4h"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "12 Часов", callback_data="mrkt_4_12h"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "1 День", callback_data="mrkt_4_1d"
-                )
-            ]
-        ]
-    )
-
-    candle_buttons_sml = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "15 Минут", callback_data="mrkt_4_15m"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "1 Час", callback_data="mrkt_4_1h"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "4 Часа", callback_data="mrkt_4_4h"
-                )
-            ]
-        ]
-    )
-
-    if plot_parameters.date_interval == 1:
-        buttons = candle_buttons_sml
-    else:
-        buttons = candle_buttons_std
-
-    bot_message = bot.send_message(chat_id=chat_id,
-                                   text=message,
-                                   reply_markup=buttons)
     last_bot_message_id = bot_message.message_id
 
 
